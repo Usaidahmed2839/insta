c52fb7f9c909017e5b4aeebce10a5ff2a7786ae5
Usaidahmed2839 <usaidahmed2388344@gmail.com>
2025-04-20 22:47:45 +0500

    Update fb.py

--- fb.py
+++ fb.py
@@ -28,12 +28,12 @@ options.add_argument(
     "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Mobile Safari/537.36"
 )
 
-# chrome_bin = os.getenv("CHROME_BIN", "/usr/bin/chromium-browser")  # Default to Chromium
-# options.binary_location = chrome_bin
+chrome_bin = os.getenv("CHROME_BIN", "/usr/bin/chromium-browser")  # Default to Chromium
+options.binary_location = chrome_bin
 
-# # Detect Chromedriver
-# chromedriver_path = os.getenv("CHROMEDRIVER_BIN", "/usr/bin/chromedriver")
-# service = Service(chromedriver_path)
+# Detect Chromedriver
+chromedriver_path = os.getenv("CHROMEDRIVER_BIN", "/usr/bin/chromedriver")
+service = Service(chromedriver_path)
 
 #  --- Facebook Scraper ---
 facebook_pages = [
